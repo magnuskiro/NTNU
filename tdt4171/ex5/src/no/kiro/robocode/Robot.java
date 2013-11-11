05ecb71cedd29915a90caf37358a273cc41e8e92
Magnus Kirø <magnuskiro@gmail.com>
2013-11-11 16:02:27 +0100

    gitignore update moved file.

---
+++ tdt4171/ex5/src/no/kiro/robocode/Robot.java
@@ -0,0 +1,11 @@
+package no.kiro.robocode;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kiro
+ * Date: 15/04/13
+ * Time: 03:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class Robot {
+}
